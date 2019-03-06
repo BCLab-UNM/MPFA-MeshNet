d9855976a055fce09ec6ae93cb3255f114427606
Vanessa Surjadidjaja <vsurjadidjaja@unm.edu>
2019-03-05 17:10:12 -0700

    Initial Commit
    Adding in CPFA/MPFA files.

---
+++ Dynamic_MPFA/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#if [ "$1" = "clean" ]; then
+    echo "Deleting and recreating the build directory "
+    rm -rf build
+    mkdir build
+#fi
+
+cd build
+
+echo "Configuring Makefiles with CMAKE..."
+cmake -DBUILD_EVOLVER=YES -DCMAKE_BUILD_TYPE=Release ..
+
+echo "Making..."
+make
+
+cd ..
+
+echo "Finished."
+
